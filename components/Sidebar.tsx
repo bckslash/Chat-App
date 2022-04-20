b9f8ae2be4aa37b5e934f0e72907dcd286508bc7
bckslash <exploreeune@gmail.com>
2022-04-20 20:49:14 +0200

    overflow scoll fix

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -50,63 +50,62 @@ function Sidebar() {
         );
 
     return (
-        <aside className="min-h-screen min-w-max border-r border-gray-300 bg-gray-100">
-            <header className="sticky z-10 flex flex-wrap items-center justify-between gap-20 border-b border-gray-300 bg-gray-200 p-2">
-                {user?.photoURL ? (
-                    <img
-                        onClick={() => auth.signOut()}
-                        className="m-2 w-10 cursor-pointer rounded-full transition-opacity duration-200 ease-in-out hover:opacity-70"
-                        src={user?.photoURL}
-                    />
-                ) : (
-                    <FaUserCircle
-                        onClick={() => auth.signOut()}
-                        className="m-3 cursor-pointer text-3xl text-gray-500 transition-opacity duration-200 ease-in-out hover:opacity-70"
-                    />
-                )}
-                <div className="flex items-center justify-center gap-2">
-                    <DefaultButton>
-                        <BsFillChatLeftTextFill className="text-xl text-primary_dark" />
-                    </DefaultButton>
-                    <DefaultButton>
-                        <BsThreeDotsVertical className="text-xl text-primary_dark" />
-                    </DefaultButton>
-                </div>
-            </header>
-            <main>
+        <aside className="scroll-hide h-screen min-w-max overflow-scroll border-r border-gray-300 bg-gray-100">
+            <main className="sticky top-0 bg-gray-100 shadow-sm">
+                <header className="z-10 flex flex-wrap items-center justify-between gap-20 border-b border-gray-300 bg-gray-200 p-2">
+                    {user?.photoURL ? (
+                        <img
+                            onClick={() => auth.signOut()}
+                            className="m-2 w-10 cursor-pointer rounded-full transition-opacity duration-200 ease-in-out hover:opacity-70"
+                            src={user?.photoURL}
+                        />
+                    ) : (
+                        <FaUserCircle
+                            onClick={() => auth.signOut()}
+                            className="m-3 cursor-pointer text-3xl text-gray-500 transition-opacity duration-200 ease-in-out hover:opacity-70"
+                        />
+                    )}
+                    <div className="flex items-center justify-center gap-2">
+                        <DefaultButton>
+                            <BsFillChatLeftTextFill className="text-xl text-primary_dark" />
+                        </DefaultButton>
+                        <DefaultButton>
+                            <BsThreeDotsVertical className="text-xl text-primary_dark" />
+                        </DefaultButton>
+                    </div>
+                </header>
                 <Search />
-
                 {/* Create chat button */}
                 <div className="border-y border-gray-300 text-center transition-colors duration-200 hover:bg-gray-200">
                     <input
                         onClick={createChat}
-                        className="h-full w-full cursor-pointer p-3"
+                        className="h-full w-full cursor-pointer p-4"
                         type="button"
                         value="START A NEW CHAT"
                     />
                 </div>
-
-                {/* list of chats */}
-                <div className="">
-                    {chatsSnapshot?.docs.map((chat) => {
-                        return (
-                            <Chat
-                                key={chat.id}
-                                id={chat.id}
-                                users={chat.data().users}
-                            />
-                        );
-                    })}
-                </div>
             </main>
+
+            {/* list of chats */}
+            <div className="">
+                {chatsSnapshot?.docs.map((chat) => {
+                    return (
+                        <Chat
+                            key={chat.id}
+                            id={chat.id}
+                            users={chat.data().users}
+                        />
+                    );
+                })}
+            </div>
         </aside>
     );
 }
 
 export const Search = () => {
     return (
-        <div className="group m-2 flex items-center justify-between gap-2 rounded-3xl p-2">
-            <AiOutlineSearch className="text-2xl text-primary_dark" />
+        <div className="group m-2 flex items-center justify-between gap-2 rounded-3xl p-2 text-primary_dark">
+            <AiOutlineSearch className="text-2xl" />
             <input
                 className="flex-1 bg-inherit outline-none focus:outline-none"
                 type="text"
