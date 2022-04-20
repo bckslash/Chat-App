8d5f437d5d0ccf64db86e5257cc7521e258952df
bckslash <exploreeune@gmail.com>
2022-04-20 19:24:55 +0200

    new default collor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary_dark: "#1f2937",
+            },
+        },
+    },
+    plugins: [],
+};
