ddbf2cdb2ea13a779e49f73b2fe4f56ba11f3824
bckslash <exploreeune@gmail.com>
2022-04-27 19:45:35 +0200

    added button component with onClick functionality

--- components/DefaultButton.tsx
+++ components/DefaultButton.tsx
@@ -1,6 +1,9 @@
-function DefaultButton({ children }: any) {
+function DefaultButton({ onclick, children }: any) {
     return (
-        <button className="rounded-full p-3 text-center transition-colors duration-200 ease-in-out hover:bg-gray-300 active:bg-gray-400">
+        <button
+            onClick={onclick}
+            className="rounded-full p-3 text-center transition-colors duration-200 ease-in-out hover:bg-gray-300 active:bg-gray-400"
+        >
             {children}
         </button>
     );
